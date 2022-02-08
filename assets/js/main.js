68c8d999f07b2ccf4b0cf88d90ccd925a796b2d6
cesar cobos <cesarcoboscv@gmail.com>
2022-02-08 17:07:13 -0600

    Hero changes, adding JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,46 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+var typeText = document.getElementById("machine")
+var textToBeTyped = "Civil Engineer"
+var textToBeTypedArr = ["Civil Engineer", "Data Science", "Project Manager","Mechanical Design", "FrontEnd Dev"]
+var index = 0, isAdding = true, textToBeTypedIndex = 0
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of the text to be typed using index.
+    typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 2s before playing again
+        setTimeout(function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+        //switch to next text in text array
+        textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, isAdding ? 120 : 60)
+}
+// start animation
+playAnim()
